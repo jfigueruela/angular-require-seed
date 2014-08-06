f7c2d5bd6b7e3dee44e3352ddebda5f50b9616a6
Jose Figueruela <jose.figueruela@codebusters.es>
2014-08-06 10:41:54 +0200

    Improved builder batch file

--- builder.sh
+++ builder.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo '--------------------------------'
-echo 'Angular-require-seed Builder 0.1'
-echo '--------------------------------'
+echo '----------------------------------'
+echo 'Angular-require-seed Builder v0.1 '
+echo '----------------------------------'
 npm install;
 bower install;
+echo '----------------------------------'
+echo 'Run develop server   : gunt serve '
+echo 'Generate dist version: gunt dist  '
+echo '----------------------------------'
